f858565d23610f57e2e5099e3557ecf097e400e8
adorda <angdmz@gmail.com>
2019-08-12 16:01:43 -0300

    refactor

--- transactions/services.go
+++ transactions/services.go
@@ -1,33 +1,73 @@
 package transactions
 
+import (
+	"errors"
+	"github.com/satori/go.uuid"
+)
+
 type Transaction interface {
 	Amount() float64
-	Id() int64
 	Logic() func(amount float64) (float64, error)
 }
 
 type TransactionService interface {
 	Balance() float64
-	Transact(t *Transaction) error
+	Transact(t Transaction) (string, error)
 }
 
 type TransactionServiceMemory struct {
 	transactions map[int64]*Transaction
 	balance      float64
+	generator    IdGenerator
 }
 
-func (tsm *TransactionServiceMemory) Transact(t *Transaction) error {
+func (tsm *TransactionServiceMemory) Transact(t Transaction) (string, error) {
 	res, err := t.Logic()(tsm.Balance())
+	if err == nil {
+		tsm.balance = res
+	}
+	return tsm.generator.Generate(), err
 }
 
-func (t *TransactionServiceMemory) Balance() float64 {
-	return t.balance
+func (tsm *TransactionServiceMemory) Balance() float64 {
+	return tsm.balance
 }
 
-func NewTransactionServiceMemory() *TransactionServiceMemory {
-	return &TransactionServiceMemory{balance: 0}
+func NewTransactionServiceMemory(ig IdGenerator) *TransactionServiceMemory {
+	return &TransactionServiceMemory{balance: 0, generator: ig}
 }
 
-func NewTransactionService() *TransactionService {
-	return NewTransactionServiceMemory()
+func NewTransactionService() TransactionService {
+	return NewTransactionServiceMemory(NewUuidGenerator())
+}
+
+type IdGenerator interface {
+	Generate() string
+}
+
+type UuidGenerator struct {
+}
+
+func (u *UuidGenerator) Generate() string {
+	gen := uuid.Must(uuid.NewV4())
+	return gen.String()
+}
+
+func NewUuidGenerator() *UuidGenerator {
+	return &UuidGenerator{}
+}
+
+func ProcessTransactionType(tx TransactionDTO) (Transaction, error) {
+	if tx.Type == "debit" {
+		return NewDebit(tx.Amount), nil
+	} else if tx.Type == "credit" {
+		return NewCredit(tx.Amount), nil
+	} else {
+		return nil, errors.New("Invalid type of transaction")
+	}
+}
+
+type TransactionDTO struct {
+	Type   string  `json:"type,omitempty"`
+	Amount float64 `json:"amount,omitempty"`
 }
